7ba18cc6373c0e230ef39b9abff9fd9a86c4f80e
坂本貴宏 <ts.1103.a.a@gmail.com>
2017-11-19 18:01:56 +0900

    追従カメラ

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	public enum CAMERA_MODE {
+		FIXEDPOINT,
+		FOLLOWUP,
+		POSE
+	}
+
+	CAMERA_MODE cameraMode;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
